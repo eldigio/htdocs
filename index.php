94157cf3bcc86e0dba0e10698a5fac6609c8cfbb
ElDiGio <samuel.dgv@gmail.com>
2023-01-18 13:34:04 +0100

    test commit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <?php if (isset($_SESSION["logged"])) redirect($_SESSION["role"]) ?>
 
 <main class="container">
-  <h1 class="my-3 text-center">Home</h1>
+  <h1 class="my-3 text-center">HomePage</h1>
 </main>
 
 <?php require "partials/footer.php" ?>
